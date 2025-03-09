35dede0d3b12813bcc69b7e1991d0296906c48e7
Renaldo Jardim <21063944+Renaldo9@users.noreply.github.com>
2025-03-09 21:34:35 +0200

    added to user components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component } from '@angular/core';
-
-import { DUMMY_USERS } from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +8,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  
   get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
+    return 'assets/users/' + this.avatar;
   }
-
+  
   onSelectUser(){
     
   }
