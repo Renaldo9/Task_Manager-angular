c2dde5dddb528e6e6ab19a0cf2a682bdff380231
Renaldo Jardim <21063944+Renaldo9@users.noreply.github.com>
2025-04-07 17:54:06 +0200

    Migrated all components to modules

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  standalone: false,
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
+
 export class CardComponent {
 
 }
